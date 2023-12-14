125ad72404a5cc5390b11b6d3c8aca13a46e5a75
J_ Keyy <ashnazg1212@gmail.com>
2023-12-14 07:56:57 +0000

    feat: Update p5.js setup and draw functions
    Add 3D canvas and draw red rectangle

--- src/components/Canvas1.jsx
+++ src/components/Canvas1.jsx
@@ -10,7 +10,12 @@ export default function Canvas1() {
   );
 }
 function sketch(p5) {
-  p5.setup = () => p5.createCanvas(600, 400);
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
 
-  p5.draw = () => {};
+  p5.draw = () => {
+    // console.log(p5.pmouseX, p5.pwinMouseX);
+    p5.background(255, 0, 0);
+    p5.rect(-100, -100, 50, 50);
+    // p5.circle(-100, -100, 100);
+  };
 }
