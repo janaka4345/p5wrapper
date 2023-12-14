110edf14d943290d2b38b2cd8ce2d33253e49813
J_ Keyy <ashnazg1212@gmail.com>
2023-12-14 10:54:45 +0000

    feat: Add mousePressed function and update circle movement
    Added mousePressed function, updated setup function and added circle movement logic.

--- src/components/Canvas1.jsx
+++ src/components/Canvas1.jsx
@@ -10,12 +10,29 @@ export default function Canvas1() {
   );
 }
 function sketch(p5) {
-  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+  let speedX = p5.random(5);
+  let speedY = p5.random(5);
+  let positionX = 30;
+  let positionY = 30;
+  let radius = 50;
+  p5.setup = () => p5.createCanvas(600, 400);
+
+  p5.mousePressed = () => {
+    console.log(p5);
+  };
 
   p5.draw = () => {
-    // console.log(p5.pmouseX, p5.pwinMouseX);
     p5.background(255, 0, 0);
-    p5.rect(-100, -100, 50, 50);
+    p5.fill(0, 255, 0);
+    p5.noStroke();
+    positionX + radius / 2 > p5.width || positionX < radius / 2
+      ? (speedX *= -1)
+      : null;
+    positionY + radius / 2 > p5.height || positionY < radius / 2
+      ? (speedY *= -1)
+      : null;
+    p5.circle((positionX += speedX), (positionY += speedY), radius);
+
     // p5.circle(-100, -100, 100);
   };
 }
